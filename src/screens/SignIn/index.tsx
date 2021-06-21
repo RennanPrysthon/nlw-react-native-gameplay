9b051f21bc043ebfcde5b05dbd20cd4a07980ff2
Rennan Prysthon <rennandelcastillo@gmail.com>
2021-06-21 20:17:16 -0300

    fix: chaged text on signin screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -20,12 +20,10 @@ export const SignIn: React.FC = () => {
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Conecte-se {`\n`}e organize suas {`\n`}
-          jogatinas{`\n`}
+          Conecte-se {"\n"}e organize suas {"\n"} jogatinas {"\n"}
         </Text>
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`}
-          favoritos com seus amigos
+          Crie grupos para jogar seus games {"\n"} favoritos com seus amigos
         </Text>
         <ButtonIcon title="Entre com o Discord" />
       </View>
