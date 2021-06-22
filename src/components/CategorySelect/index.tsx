8593b3f98ba5000838a73c878093d86cd75805cb
Rennan Prysthon <rennandelcastillo@gmail.com>
2021-06-21 22:54:44 -0300

    :sparkles: feat: added category list component

---
+++ src/components/CategorySelect/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ScrollView } from "react-native";
+
+import { styles } from "./style";
+import { categories } from "../../utils/categories";
+import Category from "../Category";
+
+type Props = {
+  categorySelected: string;
+  setCategory: (categoryId: string) => void;
+};
+
+export const CategorySelect: React.FC<Props> = ({
+  categorySelected,
+  setCategory,
+}) => {
+  return (
+    <ScrollView
+      horizontal
+      style={styles.container}
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{ paddingRight: 40 }}
+    >
+      {categories.map((category) => (
+        <Category
+          key={category.id}
+          {...category}
+          checked={category.id === categorySelected}
+          onPress={() => setCategory(category.id)}
+        />
+      ))}
+    </ScrollView>
+  );
+};
