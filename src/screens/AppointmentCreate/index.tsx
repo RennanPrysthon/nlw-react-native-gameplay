8127bc81066a7d9dafbdf9cad26ec8059ff70696
Rennan Prysthon <rennandelcastillo@gmail.com>
2021-06-23 21:12:26 -0300

    :hammer: refact: fix appointment styles

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -5,7 +5,6 @@ import {
   ScrollView,
   KeyboardAvoidingView,
   Platform,
-  Modal,
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { RectButton } from "react-native-gesture-handler";
@@ -16,6 +15,7 @@ import { ModalView } from "../../components/ModalView";
 import { Guilds } from "../Guilds";
 import { GuildIcon } from "../../components/GuildIcon";
 import { SmallInput } from "../../components/SmallInput";
+import { Background } from "../../components/Background";
 import { TextArea } from "../../components/TextArea";
 
 import { styles } from "./style";
@@ -45,80 +45,84 @@ export const AppointmentCreate: React.FC = () => {
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={styles.container}
     >
-      <ScrollView>
-        <Header title="Agendar partida" />
+      <Background>
+        <ScrollView>
+          <Header title="Agendar partida" />
+          <Text
+            style={[
+              styles.label,
+              { marginLeft: 24, marginTop: 36, marginBottom: 18 },
+            ]}
+          >
+            Categoria
+          </Text>
 
-        <Text
-          style={[
-            styles.label,
-            { marginLeft: 24, marginTop: 36, marginBottom: 18 },
-          ]}
-        >
-          Categoria
-        </Text>
-
-        <CategorySelect
-          hasCheckBox
-          setCategory={setCategory}
-          categorySelected={category}
-        />
-
-        <View style={styles.form}>
-          <RectButton onPress={handleOpenModal}>
-            <View style={styles.select}>
-              {guild.icon ? <GuildIcon /> : <View style={styles.image} />}
-
-              <View style={styles.selectBody}>
-                <Text style={styles.label}>
-                  {guild.name ? guild.name : "Selecione um servidor"}
-                </Text>
-              </View>
-              <Feather
-                name="chevron-right"
-                color={theme.colors.heading}
-                size={28}
-              />
-            </View>
-          </RectButton>
-
-          <View style={styles.field}>
-            <View>
-              <Text style={styles.label}>Dia e mês</Text>
-              <View style={styles.column}>
-                <SmallInput maxLength={2} />
-                <Text style={styles.divider}>/</Text>
-                <SmallInput maxLength={2} />
-              </View>
-            </View>
-            <View>
-              <Text style={styles.label}>Hora e minuto</Text>
-              <View style={styles.column}>
-                <SmallInput maxLength={2} />
-                <Text style={styles.divider}>:</Text>
-                <SmallInput maxLength={2} />
-              </View>
-            </View>
-          </View>
-
-          <View style={[styles.field, { marginBottom: 12 }]}>
-            <Text style={styles.label}>Descriçãp</Text>
-
-            <Text style={styles.caracteresLimit}>Max 100 caracteres</Text>
-          </View>
-
-          <TextArea
-            multiline
-            maxLength={100}
-            numberOfLines={5}
-            autoCorrect={false}
+          <CategorySelect
+            hasCheckBox
+            setCategory={setCategory}
+            categorySelected={category}
           />
 
-          <View style={styles.footer}>
-            <Button title="Agendar" />
-          </View>
-        </View>
-      </ScrollView>
+          <View style={styles.form}>
+            <RectButton onPress={handleOpenModal}>
+              <View style={styles.select}>
+                {guild.icon ? <GuildIcon /> : <View style={styles.image} />}
 
+                <View style={styles.selectBody}>
+                  <Text style={styles.label}>
+                    {guild.name ? guild.name : "Selecione um servidor"}
+                  </Text>
+                </View>
+                <Feather
+                  name="chevron-right"
+                  color={theme.colors.heading}
+                  size={28}
+                />
+              </View>
+            </RectButton>
+
+            <View style={styles.field}>
+              <View>
+                <Text style={[styles.label, { marginBottom: 12 }]}>
+                  Dia e mês
+                </Text>
+                <View style={styles.column}>
+                  <SmallInput maxLength={2} />
+                  <Text style={styles.divider}>/</Text>
+                  <SmallInput maxLength={2} />
+                </View>
+              </View>
+              <View>
+                <Text style={[styles.label, { marginBottom: 12 }]}>
+                  Hora e minuto
+                </Text>
+                <View style={styles.column}>
+                  <SmallInput maxLength={2} />
+                  <Text style={styles.divider}>:</Text>
+                  <SmallInput maxLength={2} />
+                </View>
+              </View>
+            </View>
+
+            <View style={[styles.field, { marginBottom: 12 }]}>
+              <Text style={styles.label}>Descriçãp</Text>
+
+              <Text style={styles.caracteresLimit}>Max 100 caracteres</Text>
+            </View>
+
+            <TextArea
+              multiline
+              maxLength={100}
+              numberOfLines={5}
+              autoCorrect={false}
+            />
+
+            <View style={styles.footer}>
+              <Button title="Agendar" />
+            </View>
+          </View>
+        </ScrollView>
+      </Background>
       <ModalView visible={visible} closeModal={handleCloseModal}>
         <Guilds handleGuildSelect={handleGuildSelect} />
       </ModalView>
